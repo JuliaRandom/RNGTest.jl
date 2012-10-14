a86f977daef2c666b11c1fa4220ec63185b70dc2
Viral B. Shah <viral@mayin.org>
2012-10-14 23:00:28 +0530

    Add randmtzig.c and Phi(x) to test randn. Update crushtests.c Add Makefile

--- crushtests.c
+++ crushtests.c
@@ -1,11 +1,26 @@
-#include "bbattery.h"
-#include "dsfmt-2.2/dSFMT.c"
-#include "randmtzig.c"
+#include "./deps/TestU01-1.2.3/include/bbattery.h"
+#include "./deps/dsfmt-2.2/dSFMT.c"
+#include "./deps/randmtzig.c"
 
+/* From Rmath
 double pnorm5(double, double, double, int, int);
 double randntorand(void)
 {
 	return pnorm5(randmtzig_randn(), 0.0, 1.0, 1, 0);
+}
+*/
+
+// From Marsaglia 2004
+double Phi(double x)
+{
+    long double s=x,t=0,b=x,q=x*x,i=1;
+    while(s!=t) s=(t=s)+(b*=q/(i+=2));
+    return .5+s*exp(-.5*q-.91893853320467274178L);
+}
+
+double randntorand(void)
+{
+    return Phi(randmtzig_randn());
 }
 
 int main (void)
