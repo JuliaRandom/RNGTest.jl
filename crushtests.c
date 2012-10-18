35d883cd417a213302e111d9c0f4e15a5b95aacd
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2012-10-18 14:32:27 +0200

    Added tests for uniform and exponential distributions. Copied a fresh testu01 as I could not build on Ubuntu.

--- crushtests.c
+++ crushtests.c
@@ -1,37 +1,70 @@
 #include "./deps/TestU01-1.2.3/include/bbattery.h"
+#include "swrite.h"
 #include "./deps/dsfmt-2.2/dSFMT.c"
 #include "./deps/randmtzig.c"
 
-/* From Rmath
-double pnorm5(double, double, double, int, int);
-double randntorand(void)
-{
-	return pnorm5(randmtzig_randn(), 0.0, 1.0, 1, 0);
-}
-*/
-
-// From Marsaglia 2004
+// Normal dist. From Marsaglia 2004
 double Phi(double x)
 {
     long double s=x,t=0,b=x,q=x*x,i=1;
     while(s!=t) s=(t=s)+(b*=q/(i+=2));
     return .5+s*exp(-.5*q-.91893853320467274178L);
 }
-
-double randntorand(void)
+double randntoU01(void)
 {
     return Phi(randmtzig_randn());
 }
+// End normal
+
+// Exponential dist.
+double pexp(double x)
+{
+	return 1.0 - exp(-x);
+}
+double exprndtoU01(void)
+{
+	return pexp(randmtzig_exprnd());
+}
+// End Exponential
+
+// Naive exponential dist. Just to test that one works.
+double naive_exprnd(void)
+{
+	return -log(1.0 - randu());
+}
+double naive_exprndtoU01(void)
+{
+	return pexp(naive_exprnd());
+}
+// End Naive exp.
 
 int main (void)
 {
 	dsfmt_gv_init_gen_rand(117);
 	randmtzig_create_ziggurat_tables();
 	unif01_Gen *gen;
-	// swrite_Basic = FALSE;
-	gen = unif01_CreateExternGen01 ("randmtzig_randn", randntorand);
-	bbattery_SmallCrush (gen); 	// 30 second test
-	// bbattery_Crush (gen); 		// 2 hour test
+	swrite_Basic = FALSE;
+	
+	// Uniform distribution
+	gen = unif01_CreateExternGen01("randu", randu);
+	bbattery_SmallCrush(gen); 		// 30 seconds test
+	unif01_DeleteExternGen01(gen);
+
+	// Normal ditribution
+	gen = unif01_CreateExternGen01("randmtzig_randn", randntoU01);
+	bbattery_SmallCrush(gen); 		// 30 seconds test
+	unif01_DeleteExternGen01(gen);
+
+	// Exponential distribution
+	gen = unif01_CreateExternGen01("randmtzig_exprnd", exprndtoU01);
+	bbattery_SmallCrush(gen); 		// 30 seconds test
+	unif01_DeleteExternGen01(gen);
+
+	gen = unif01_CreateExternGen01("naive_exprnd", naive_exprndtoU01);
+	bbattery_SmallCrush(gen); 		// 30 seconds test
+	unif01_DeleteExternGen01(gen);
+
+	// bbattery_Crush (gen); 		// 2 hours test
 	// bbattery_Crush (gen);		// Many hours test
 	return 0;
 }
