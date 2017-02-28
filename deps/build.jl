504425ad1badb6cf98d9a6cb9f910c03027c3890
GregPlowman <GregPlowman@users.noreply.github.com>
2017-02-28 11:41:08 +1100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,9 +4,9 @@ cd(dirname(@__FILE__)) do
             run(`mingw32-make`)
         catch
             if Sys.WORD_SIZE == 32
-                url = "https://github.com/GregPlowman/RNGTest.jl/releases/download/TestU01/libtestu01wrapper-32.dll"
+                url = "https://github.com/GregPlowman/RNGTest.jl/deps/TestU01/bin/libtestu01wrapper-32.dll"
             else
-                url = "https://github.com/sunoru/RandomNumbers.jl/releases/download/deplib-0.1/librandom123.dll"
+                url = "https://github.com/GregPlowman/RNGTest.jl/deps/TestU01/bin/libtestu01wrapper-64.dll"
             end
             info("MinGW32 not installed, now downloading the library binary from github.")
             download(url, "libtestu01wrapper.dll")
