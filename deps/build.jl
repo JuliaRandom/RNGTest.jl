14f5135338009389a76f184902f5ae31754e1c14
GregPlowman <GregPlowman@users.noreply.github.com>
2017-02-28 19:46:40 +1100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -5,11 +5,11 @@ cd(dirname(@__FILE__)) do
         catch
             info("Cannot run make, installing binary instead")
             if Sys.WORD_SIZE == 32
-                srcfile = "TestU01/bin/libtestu01wrapper-32.dll"
+                binfile = "TestU01/bin/libtestu01wrapper-32.dll"
             else
-                srcfile = "TestU01/bin/libtestu01wrapper-64.dll"
+                binfile = "TestU01/bin/libtestu01wrapper-64.dll"
             end
-            cp(srcfile, "libtestu01wrapper.dll")
+            cp(binfile, "libtestu01wrapper.dll")
         end
     else
         run(`make`)
